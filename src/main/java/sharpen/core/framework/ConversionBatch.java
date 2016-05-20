08e5b72102fe1c005f744320d0f032d4810a02e3
HankG <hankgrabowski@gmail.com>
2016-05-19 23:19:59 -0400

    Will now compile lambda expressions, but not correctly yet

--- src/main/java/sharpen/core/framework/ConversionBatch.java
+++ src/main/java/sharpen/core/framework/ConversionBatch.java
@@ -24,10 +24,10 @@ public abstract class ConversionBatch {
 		
 		@SuppressWarnings("unchecked")
 		Map<String, String> options = JavaCore.getOptions();
-		options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_7);
+		options.put(JavaCore.COMPILER_COMPLIANCE, JavaCore.VERSION_1_8);
 		options.put(JavaCore.COMPILER_CODEGEN_TARGET_PLATFORM,
-				JavaCore.VERSION_1_7);
-		options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_7);
+				JavaCore.VERSION_1_8);
+		options.put(JavaCore.COMPILER_SOURCE, JavaCore.VERSION_1_8);
 		_parser.setCompilerOptions(options);
 	}
 
